5a58e5dc018c12338209841d99c128b69760956a
Man <manishgodhani.tp@gmail.com>
2024-01-27 01:36:25 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -3,17 +3,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 mongoose
-    .connect(
-        process.env.MONGO
-    ).then(()=>{
-        console.log('MongoDB is connected');
-    }).catch(
-        (err) =>{
-            console.log(err);
-        }
-    );
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
