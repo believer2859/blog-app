dcc2f7834c632223fa54bc0a7cfcbdc718ceb7b0
Man <manishgodhani.tp@gmail.com>
2024-03-04 03:59:39 +0530

    complete search page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navbar,TextInput,Button, Dropdown, Avatar } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import{ AiOutlineSearch } from 'react-icons/ai';
 import {FaMoon, FaSun }from 'react-icons/fa';
 import { useSelector,useDispatch } from 'react-redux';
@@ -9,9 +9,21 @@ import {signoutSuccess} from '../redux/user/userSlice';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
   const { currentUser } = useSelector(state => state.user);
   const dispatch = useDispatch();
   const { theme } = useSelector(state => state.theme);
+  const [searchTerm, setSearchTerm] = useState('');
+  console.log(searchTerm);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  },[location.search]);
+
   const handleSignout = async () => {
     try {
       const res = await fetch('/api/user/signout', {
@@ -27,6 +39,13 @@ export default function Header() {
       console.log(error.message);
     }
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new  URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
 
   return (
     <Navbar className='border-b-2' >
@@ -34,19 +53,21 @@ export default function Header() {
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white" >Journey</span>
               In Frames
         </Link>
-        <form>
+        <form onSubmit={handleSubmit}>
           <TextInput 
             type='text'
             placeholder='Search...'
             rightIcon={AiOutlineSearch}
             className='hidden lg:inline'
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
           />
         </form>
         <Button className='w-12 h-10 lg:hidden' color='gray' pill>
           <AiOutlineSearch />
         </Button>
         <div className='flex gsp-2 md:order-2' >
-          <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={() => dispatch(toggleTheme())}>
+          <Button className='w-12 h-10 hidden m-2 sm:inline' color='gray' pill onClick={() => dispatch(toggleTheme())}>
           { theme === 'light' ? <FaSun /> : <FaMoon /> }  
         </Button>
         { currentUser ? (
